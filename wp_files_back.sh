3ecffcda8952bac25b1d5f624bfc77e6e2ade2bf
mig <mig@VIENB031Z042.tah.insite>
2017-02-28 12:30:26 +0100

    change backup dir

--- wp_files_back.sh
+++ wp_files_back.sh
@@ -1,13 +1,34 @@
 #!/bin/sh
- 
+UNSET DEBUG
+#Uncomment the following line to get debuging ouput
+#DEBUG="TRUE"
+
+#det the path to your install
 BACKUP_FILES="/usr/share/wordpress"
-DEST="/srv/wordpress/backup"
-TEMP="/tmp/
+#Backupdirectory
+DEST="/srv/backup/wordpress"
+#temp inside the Backupdirectory
+TEMP=$DEST"/tmp/"
+
+function debugecho()
+{ 
+    if [ ! -z "$DEBUG" ] then echo "$*" fi 
+}
+
+if [ ! -d $TEMP ]
+then
+    mkdir -p $TEMP ; debugecho "Directory $TEMP created." ||  debugecho "Error: Failed to create $TEMP directory."
+else
+    debugecho "$TEMP directory exits!"
+fi
  
 DAY=$(date +%A)
 HOST=$(hostname -s)
 FILENAME="WP_FILES$HOST-$DAY.tgz"
  
 tar czf $TEMP$FILENAME $BACKUP_FILES
-rotate_backups "-d $DEST" "-s $TEMP" "-f $FILENAME"
+./rotate_backups "-d $DEST" "-s $TEMP" "-f $FILENAME"
+debugecho "rotate Backup invoked"
+rm -f $TEMP
+
 
